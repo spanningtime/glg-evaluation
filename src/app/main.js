38e539a82a6dcd238e0f9351b07dd4898f7e8f5f
William Knight <kniwilliam@gmail.com>
2017-02-16 14:10:04 -0800

    add extra media query and clean up jQuery

--- src/app/main.js
+++ src/app/main.js
@@ -1,67 +1,61 @@
-(function() {
-  $(document).ready(function() {
-    $('.carousel').carousel()
-    $('.dropdown-toggle').dropdown()
+'use strict';
+
+$(document).ready(function() {
+  $('.carousel').carousel();
+});
+
+const $tilesContainer = $('.tiles-container');
+const $overlays = $('.overlay');
+
+function addOverlay(target) {
+  target.css('opacity', '1');
+}
+
+// Removes all overlays as well as the active overlay that is clicked on
+function removeOverlay(target) {
+  if (target.hasClass('overlay')) {
+    target.eq(0).css('opacity', '0');
+  }
+  $overlays.each((i, obj) => {
+    $(obj).css('opacity', '0');
   });
+}
 
-  const $tilesContainer = $(".tiles-container");
-  const $tiles = $(".tile");
-  const $overlays = $(".overlay");
+// Handles adding/removing overlay if click target is overlay text
+function handleChildrenOverlay(target) {
+  const overlay = target.parent('div');
 
+  if (overlay.css('opacity') === '0') {
+    removeOverlay(target);
+    overlay.css('opacity', '1');
+  }
 
-  const addOverlay = function(target) {
-    target.css('opacity', '1');
-  };
+  else {
+    overlay.css('opacity', '0');
+  }
+}
 
-  const removeOverlay = function(target) {
-    if (target.hasClass("overlay")) {
-      target.eq(0).css('opacity', '0')
-    }
-    $overlays.each((i, obj) => {
-      $(obj).css("opacity", "0");
-    })
-  };
+// Checks click to see if target is the overlay-text or the overlay itself & calls appropriate functions
+function checkOverlay(target) {
+  if (target.hasClass('overlay-text')) {
+    handleChildrenOverlay(target);
+  }
 
-  const handleChildrenOverlay = function(target) {
-    const overlay = target.parent("div");
+  else if (target.css('opacity') === '1') {
+    removeOverlay(target);
+  }
 
-    if (target.hasClass("overlay-text")) {
+  else {
+    removeOverlay(target);
+    addOverlay(target);
+  }
+}
 
-      if (overlay.css("opacity") === "0") {
-        removeOverlay(target);
-        overlay.css("opacity", "1")
-      }
-      else {
-        overlay.css("opacity", '0');
-      }
-    }
-
-    else if (overlay.css("opacity", "1")) {
-      overlay.css("opacity", "0");
-    }
-  };
-
-  const checkOverlay = function(target) {
-    if (target.hasClass("overlay-text")) {
-      handleChildrenOverlay(target)
-    }
-
-    else if (target.css("opacity") === '1') {
-      removeOverlay(target);
-    }
-
-    else {
-      removeOverlay(target);
-      addOverlay(target);
-    }
-  };
-
-  $tilesContainer.click((event) => {
-    if (window.innerWidth > 768) {
-      return;
-    }
-    let $target = $(event.target)
-    checkOverlay($target);
-  })
-
-})();
+/* click event for tile overlays */
+$tilesContainer.on('click', (event) => {
+  if (window.innerWidth > 768) {
+    return;
+  }
+  const $target = $(event.target);
+  checkOverlay($target);
+});
